5d44c7f288b33b18dc47ef22d2a69c9d7bb48bf0
TyroneMartin <tyrone306@byui.edu>
2024-01-20 23:16:27 -0700

    update

--- routes/static.js
+++ routes/static.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
+// router.use("/css", express.static(__dirname + "newrepo/public/css/styles.css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
